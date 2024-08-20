0dafa9427e124bf2701ec27e9aed24979fa765e7
shafran <shafran.andriy@gmail.com>
2024-08-20 22:12:02 +0300

    Created all 3 function for convert report info

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
-def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+from typing import Any
 
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+def format_linter_error(error: dict) -> dict[Any, Any]:
+    return {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
+        "source": "flake8"
+    }
 
 
-def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+def format_single_linter_file(file_path: str,
+                              errors: list[dict]) -> dict:
+    return {"errors": [
+        format_linter_error(error) for error in errors
+    ],
+        "path": file_path,
+        "status": "failed" if any(errors) else "passed"
+    }
+
+
+def format_linter_report(linter_report: dict) -> list[dict]:
+    return [
+        format_single_linter_file(key, value)
+        for (key, value) in linter_report.items()
+    ]
